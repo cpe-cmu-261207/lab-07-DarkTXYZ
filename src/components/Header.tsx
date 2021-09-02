a8caf0fb3cff4d695677857594ea73c6e6d05e39
DarkTXYZ <pawaretmontfort@gmail.com>
2021-09-02 15:17:26 +0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div>
       <p className='text-center text-3xl italic my-2'>Pixel Painter</p>
-      <p className='text-center'>by Chayanin Suatap 610631100</p>
+      <p className='text-center'>by Pawaret Dilokwuttisit 630610748</p>
     </div>
   )
 }
